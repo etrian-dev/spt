3680acc3ebefa465b2211f39c6af6ed7278a0c78
etrian-dev <nicola.vetrini@gmail.com>
2020-11-24 23:20:55 +0100

    Started the transition to using glib data structures such as GSlist and GQueue Added a Makefile as well to simplify compilation
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	deleted:    bellman-ford
    	modified:   input0.txt
    	modified:   spt.l
    	modified:   spt.l.c
    
     Untracked files:
    	.vscode/
    	Makefile
    	graph.o
    	queue.o
    	spt.l.o

--- spt.l.c
+++ spt.l.c
@@ -2,12 +2,40 @@
 // by using the Bellman-Ford algorithm (uses a queue)
 
 // my headers
-#include "queue.h"
 #include "graph.h"
+
+//Glib headers for the queue and the list
+#include <glib.h>
+
+// readline is used to read the input graph
+#include <readline/readline.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+
+/* The graph node */
+typedef struct node_t {
+    int destination;
+    float weight;
+} Node;
+
+void destroy_node(gpointer data) {
+    free(data);
+}
+
+void print_graph(GSlist *graph, const int order) {
+    int i, j;
+    for (i = 0; i < order; i++) {
+        printf("%d -> ", i);
+        
+        for (j = 0; j < g_slist_length() - 1; j++) {
+            printf("%d (%d), ", G[i].adjacent[j], G[i].weights[j]);
+        }
+        printf("%d (%d)\n", G[i].adjacent[j], G[i].weights[j]);
+    }
+    putchar('\n');
+}
 
 int main(void) {
     // reads the vertices and edges, then the graph
@@ -16,11 +44,49 @@ int main(void) {
     printf("Enter the number of vertices and edges: ");
     scanf("%d %d", &vertices, &edges);
 
-    Graph G = read_graph(vertices, &max_w);
+    // The graph is an adjacency matrix (list of lists)
+    //Graph G = read_graph(vertices, &max_w);
+    GSlist *graph = NULL;   // initialized as the empty list
+    // read the vertex's adjacency list using readline()
+    char *line = readline(NULL);
+    GSlist *adjlist = NULL;
+    int dest = -1;
+    float weight = 0;
+    Node *n = NULL;
+    while(line) {
+        // parse the adjacency list in a list
+        while(scanf(" %d:%f", &dest, &weight) == 2) {
+            // allocates memory for the pointer to the Node and fills its records
+            n = (Node*)malloc(sizeof(Node));
+            if(n == NULL) {
+                puts("Failed to alloc Node");
+                exit(1);
+            }
+            n->destination = dest;
+            n->weight = weight;
+            // prepends the Node n to the list
+            adjlist = g_slist_prepend(adjlist, n);
+        }
+        graph = g_slist_prepend(graph, adjlist);
+        
+        // then the list is cleared and its head pointer set to NULL to be safe
+        g_clear_slist(&adjlist, destroy_node);
+        adjlist = NULL;
+        
+        // input line freed
+        free(line);
+        // a new line is then read from input
+        line = readline(NULL);
+    }
+    // input line freed
+    free(line);
+    // then the list is cleared and its head pointer set to NULL to be safe
+    g_clear_slist(&adjlist, destroy_node);
+    adjlist = NULL;
 
     // prints the graph
     puts("GRAPH");
-    print_graph(G, vertices);
+    print_graph(graph, vertices);
 
     // choose the root node
     int root, i;
@@ -28,8 +94,11 @@ int main(void) {
     scanf("%d", &root);
 
     // fifo list Q
-    Queue Qhead = NULL;
-    Queue Qtail = NULL;
+    //Queue Qhead = NULL;
+    //Queue Qtail = NULL;
+    
+    // defines and initializes a queue
+    GQueue *Q = g_queue_new();
 
     // each node i has a label (the cost of the path from the root to i)
     int *labels = (int*) malloc(vertices * sizeof(int));
@@ -49,39 +118,46 @@ int main(void) {
     }
     // after initializing the tree, the tail elements of those edges who violate bellman conditions
     // must be inserted in Q. In this case, just the root is violating them, because of how the tree is built
-    enqueue(&Qhead, &Qtail, root);
+    g_queue_push_tail(Q, GINT_TO_POINTER(root));
+    printf("inserted %d\n", root);
 
-    Element* u = NULL;
+    //Element* u = NULL;
+    int u = 0;
     int count_it = 0; // just counts the iterations
+    
     // while Q is not empty, loop
-    while(Qhead != NULL) {
+    while(!g_queue_is_empty(Q)) {
+        // updates the number of iterations
         count_it++;
-        // in Bellman-Ford Q is FIFO and at each iteration Q is dequeued
-        u = dequeue(&Qhead, &Qtail);
+        
+        // in Bellman-Ford Q is FIFO and dequeued at each iteration
+        u = GPOINTER_TO_INT(g_queue_pop_head(Q));
+        u = g_slist_find(graph, 
 
-        // check each forward edge from u->value
-        for (i = 0; i < G[u->value].out_degree; i++) {
+        // check each forward edge from u
+        for (i = 0; i < G[u].out_degree; i++) {
 
-            // check if the edge (u->value, i) violates the corresponding Bellman condition
-            if (labels[G[u->value].adjacent[i]] > labels[u->value] + G[u->value].weights[i]) {
-                printf("(%d, %d) violates Bellman\n", u->value, G[u->value].adjacent[i]);
-                printf("d_%d\t+\tc_%d_%d\t<\td_%d\n", u->value, u->value, G[u->value].adjacent[i], G[u->value].adjacent[i]);
-                printf("%d\t+\t%d\t<\t%d\n", labels[u->value], G[u->value].weights[i], labels[G[u->value].adjacent[i]]);
+            // check if the edge (u, i) violates the corresponding Bellman condition
+            if (labels[G[u].adjacent[i]] > labels[u] + G[u].weights[i]) {
+                printf("(%d, %d) violates Bellman\n", u, G[u].adjacent[i]);
+                printf("d_%d\t+\tc_%d_%d\t<\td_%d\n", u, u, G[u].adjacent[i], G[u].adjacent[i]);
+                printf("%d\t+\t%d\t<\t%d\n", labels[u], G[u].weights[i], labels[G[u].adjacent[i]]);
 
                 // update labels[i] (not its tree!) and its predecessor if it changes
-                labels[G[u->value].adjacent[i]] = labels[u->value] + G[u->value].weights[i];
-                if (predecessors[G[u->value].adjacent[i]] != u->value) {
-                    predecessors[G[u->value].adjacent[i]] = u->value;
+                labels[G[u].adjacent[i]] = labels[u] + G[u].weights[i];
+                if (predecessors[G[u].adjacent[i]] != u) {
+                    predecessors[G[u].adjacent[i]] = u;
                 }
-                // inserts i in Q if it's not already in there
-                if (!isIn(Qhead, G[u->value].adjacent[i])) {
-                    enqueue(&Qhead, &Qtail, G[u->value].adjacent[i]);
+                // inserts G[u].adjacent[i] in Q if it's not already in there
+                if (g_queue_find(Q, GINT_TO_POINTER(G[u].adjacent[i])) == NULL) {
+                    g_queue_push_tail(Q, GINT_TO_POINTER(G[u].adjacent[i]));
                 }
             }
         }
-        // frees the element extracted from the queue
-        free(u);
     }
+    
+    // free the queue (no dynamically alloc'd data stored
+    g_queue_free(Q);
 
     printf("After %d iterations, the SPT found by Bellman-Ford is\n", count_it);
     // the resulting spt is represented by labels & predecessors
